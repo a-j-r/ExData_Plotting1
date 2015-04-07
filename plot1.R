b2f3af283a6b9b88807843aa4ccc7271640eb116
a-j-r <a-j-r@users.noreply.github.com>
2015-04-07 15:38:14 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,17 +1,24 @@
-
+# Remove eventual unnecessary data
 rm(list = ls())
+
+# Read the data
 if (!file.exists("hpc.rds")) {
 household_power_consumption <- read.csv("household_power_consumption.txt",sep=";", quote = "\"", header=TRUE, colClasses=c("character", "character", rep("numeric",7)), na.strings=c("?") )
-household_power_consumption[[1]] <- as.Date(household_power_consumption[[1]], "%d/%m/%Y")
 
+# Transform data as needed.
+household_power_consumption[[1]] <- as.Date(household_power_consumption[[1]], "%d/%m/%Y")
 household_power_consumption <- household_power_consumption[household_power_consumption$Date >= as.Date("2007-02-01", "%Y-%m-%d") & household_power_consumption$Date <= as.Date("2007-02-02", "%Y-%m-%d"),]
 household_power_consumption[[2]] <- strptime(paste(household_power_consumption[[1]], household_power_consumption[[2]]), "%Y-%m-%d %H:%M:%S", tz="")
 
+#save modified data and clean memory
 saveRDS(household_power_consumption, file = "hpc.rds")
 rm(list = ls())
 }
+
+# Read cleaned data to display
 hpc <- readRDS("hpc.rds")
 
+# Create png-file with the diagram.
 png("plot1.png", width=480, height=480)
 hist(hpc$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowats)")
 dev.off()
